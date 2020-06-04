aa2fdb6e741ec0a49819cac1a6cff78976bc29ca
lijovijayan <lijovijayan00@gmail.com>
2020-06-04 22:42:57 +0530

    installed axios - and updated app code

--- node/app.js
+++ node/app.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
@@ -6,10 +7,29 @@ const parser = port.pipe(new Readline({ delimiter: '\n' }));
 port.on("open", () => {
   console.log('serial port open');
 });
-parser.on('data', (distance) =>{
-  if( distance > 50) {
+var numberOfObjectsDetected = 0;
+var isCounterActive = true;
+parser.on('data', async (distance) => {
+  if (distance > 50) {
+    isCounterActive = true;
     console.log('no objects object detected, distance: ', distance);
   } else {
+    await incrementCount();
     console.log('object detected, distance: ', distance);
   }
 });
+const incrementCount = async () => {
+  if (!isCounterActive) {
+    return;
+  }
+  var data = {
+    numberOfObjectsDetected: numberOfObjectsDetected,
+  }
+  await axios.post('http://localhost:3000/', data).then((data) => {
+    console.log(data.data);
+  }).catch((error) => {
+    console.log(error);
+  });
+  numberOfObjectsDetected++;
+  isCounterActive = false;
+}
