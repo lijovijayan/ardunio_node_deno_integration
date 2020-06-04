2d5095bfbf7f07c0afe9138dde3d37a815e4d49f
lijovijayan <lijovijayan00@gmail.com>
2020-06-04 22:42:28 +0530

    updated routes

--- deno/routes/root.routes.ts
+++ deno/routes/root.routes.ts
@@ -1,7 +1,20 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import { log, MESSAGE } from "../utils/log.utils.ts";
 const router = new Router();
-router.get('/', (context) => {
-    context.response.status = 200;
-    context.response.body = 'on root';
-});
+router.get("/", (context) => {
+  console.log("get request on root");
+  context.response.status = 200;
+  context.response.body = "on root (get)";
+})
+  .post("/", async ({ request, response }: { request: any; response: any }) => {
+    if (request.hasBody) {
+      const body = await request.body();
+      const data = body.value;
+      const message = `No. of Persons entered: ${data.numberOfObjectsDetected}`
+      log('on root (post request)', MESSAGE.bold);
+      log(message, MESSAGE.url);
+      response.status = 200;
+      response.body = "on root (post)";
+    }
+  });
 export default router;
