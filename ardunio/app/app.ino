8a42fa5a5e938c22e043ac3425d88ab9c9948f55
lijovijayan <lijovijayan00@gmail.com>
2020-06-04 19:45:42 +0530

    ardunio code

---
+++ ardunio/app/app.ino
@@ -0,0 +1,29 @@
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+
+void setup() {
+   Serial.begin(9600); // Starting Serial Terminal
+   pinMode(pingPin, OUTPUT);
+   pinMode(echoPin, INPUT);
+}
+
+void loop() {
+   long duration, inches, cm;
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   duration = pulseIn(echoPin, HIGH);
+   cm = microsecondsToCentimeters(duration);
+   Serial.println(cm);
+   delay(100);
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
